2e6a8bfb791fd7f81be6ec7013da1d0d1dab25da
Tursynkhan <tursynkhan001@gmail.com>
2023-07-18 11:20:24 +0600

    fix typo

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { usePathname,useRouter } from "next/Navigation";
+import { usePathname,useRouter } from "next/navigation";
 
 const PromptCard = ({post,handleTagClick,handleEdit,handleDelete}) => {
 
